010838e55bf196dc597591118888a04827854d80
Ray Kang <adamck6302438@gmail.com>
2019-05-20 17:36:18 -0400

    Initial Commit

---
+++ InstaKilo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaKilo
+//
+//  Created by Frances ZiyiFan on 5/20/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
